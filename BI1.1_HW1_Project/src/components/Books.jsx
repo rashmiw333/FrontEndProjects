a56801eca0f4302a48aafd0cf7ea7d0f1079d656
rashmiw333 <92781108+rashmiw333@users.noreply.github.com>
2026-06-26 10:12:51 +0530

    Update Books.jsx

--- BI1.1_HW1_Project/src/components/Books.jsx
+++ BI1.1_HW1_Project/src/components/Books.jsx
@@ -3,7 +3,7 @@ import useFetch from "../useFetch"
 const Books =()=>{
 
     const{data, loading, error} = 
-    useFetch("http://localhost:3000/books");
+    useFetch("backend-books-tau.vercel.app/books");
 
    // console.log(data);
 
