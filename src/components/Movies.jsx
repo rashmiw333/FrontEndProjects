9a6a92b4ebeaaab1a99b27e3d4a09cdfa010ac46
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-29 16:18:58 +0530

    delete opertion

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import useFetch from "../useFetch"
 
 const Movies =()=>{
-
+    const [successMessage,setSuccessMessage] = useState("");
     const{data, loading, error} = 
     useFetch("http://localhost:3000/movies");
 
-    // console.log(data);
+    const handleDelete =async(movieId)=>{
+        try{
+        const response = await fetch(`http://localhost:3000/movies/${movieId}`,
+            {method: "DELETE"},
+        );
+        if(!response.ok){
+            throw "Fai;ed to delete a Movie"
+        }
+
+        const data = await response.json();
+            if(data){
+            setSuccessMessage("Movie deleted Suceessfully!");
+                window.location.reload();
+            }
+        }catch(error){
+            console.log(error)
+        }
+    }
 
     return(
         <div className="movies-container">
@@ -13,9 +31,12 @@ const Movies =()=>{
             {error&& <P>{error}</P>}
             <ul>
             {data?.map(movie=>(
-                <li key={movie._id}>{movie.title}</li>
+                <li key={movie._id}>{movie.title}{" "}
+                <button onClick={()=>handleDelete(movie._id)}>Delete</button>
+                </li>
             ))}
             </ul>
+            <p>{successMessage}</p>
         </div>
     )
 }
