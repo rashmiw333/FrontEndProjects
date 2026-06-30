bdb0486466ceef2cd9588802cf54681b4eb69ed2
rashmiw333 <92781108+rashmiw333@users.noreply.github.com>
2026-06-30 12:25:59 +0530

    Update useFetch for Books

--- BI1.2_HW1/src/useFetch.jsx
+++ BI1.2_HW1/src/useFetch.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-const useFetch = (url, initialData) => {
+const useFetch = (url, initialData = []) => {
   const [data, setData] = useState(initialData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
